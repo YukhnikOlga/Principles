2a8263dc87cdfc169b09b7853e0c01e264f7a034
YukhnikOlga <olga.iukhnik@gmail.com>
2024-05-28 11:26:57 +0300

    Initial commit

---
+++ src/main/java/post.java
@@ -0,0 +1,9 @@
+public class post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phohe;
+    public String surname;
+    public boolean subscription;
+    public FormDate birthday;
+}
